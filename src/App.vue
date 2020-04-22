ffc93500fd3fe7c5a5f4d2ac9048f707ddf10e43
karen <gallegokaren1897@gmail.com>
2020-04-22 18:56:55 -0400

    lint files

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png"/>
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-        <Timer/>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Timer />
+  </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import HelloWorld from "./components/HelloWorld.vue";
-    import Timer from "./components/Timer.vue";
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import Timer from "./components/Timer.vue";
 
-    @Component({
-        components: {
-            HelloWorld,
-            Timer
-        }
-    })
-    export default class App extends Vue {
-    }
+@Component({
+  components: {
+    HelloWorld,
+    Timer
+  }
+})
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="hello">
-        <button @click="show = true">click me</button>
-        <div v-if="show">
-            TACOOOS
-        </div>
+  <div class="hello">
+    <button @click="show = true">click me</button>
+    <div v-if="show">
+      TACOOOS
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue} from "vue-property-decorator";
-    import {ipcRenderer} from "electron";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { ipcRenderer } from "electron";
 
-    @Component
-    export default class HelloWorld extends Vue {
-        private show = false;
+@Component
+export default class HelloWorld extends Vue {
+  private show = false;
 
-        created() {
-            ipcRenderer.on('showTheStuff', (e, args) => {
-                this.show = true;
-            });
-        }
-    }
+  created() {
+    ipcRenderer.on("showTheStuff", (e, args) => {
+      this.show = true;
+    });
+  }
+}
 </script>
