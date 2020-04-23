8a54ddb848bdd899700198acb73472493494cfe6
karen <gallegokaren1897@gmail.com>
2020-04-23 12:15:54 -0400

    add goals section style

--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    Goals
+  <div class="container">
+    <label for="swell">Swell</label>
+    <br />
+    <input id="swell" type="text">
+
+    <br />
+
+    <label for="swole">Swole</label>
+    <br />
+    <input id="swole" type="text">
   </div>
 </template>
 
@@ -8,6 +16,13 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Goals extends Vue {
-}
+export default class Goals extends Vue {}
 </script>
+
+<style lang="scss">
+.container {
+  box-shadow: 15px 4px 54px rgba(0, 0, 0, 0.05);
+  width: 33%;
+  height: 100vh;
+}
+</style>
