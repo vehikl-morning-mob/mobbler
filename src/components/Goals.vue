99aa88300d090bde78f1ee1f9ddefc7a1ad80864
vehikl <vehikl@vehikl.com>
2020-05-14 16:56:02 -0400

    Add margin to goals list

--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container">
-    <label for="swell">Swell</label>
-    <br />
-    <input id="swell" type="text">
+    <fieldset>
+      <label for="swell">Swell</label>
+      <br />
+      <input id="swell" type="text" placeholder="Good day">
+    </fieldset>
 
     <br />
 
-    <label for="swole">Swole</label>
-    <br />
-    <input id="swole" type="text">
+    <fieldset>
+      <label for="swole">Swole</label>
+      <br />
+      <input id="swole" type="text" placeholder="Great day">
+    </fieldset>
   </div>
 </template>
 
@@ -19,14 +23,19 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Goals extends Vue {}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .container {
   box-shadow: 15px 4px 54px rgba(0, 0, 0, 0.05);
   min-width: 300px;
   width: 33%;
   height: 100vh;
+  padding: 2rem;
   label {
     font-weight: bold;
+    margin: 0.5rem 0;
+  }
+  fieldset {
+    margin-bottom: 1rem;
   }
 }
 
